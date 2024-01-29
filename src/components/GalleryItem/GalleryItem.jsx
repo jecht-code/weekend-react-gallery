a29036c6b3408a1e4ff2b810c1e95e93d7e2f2a0
Scott Pak <gpak86@gmail.com>
2024-01-29 18:38:18 -0500

    finished the click image and changing to description

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React from 'react';
-
+import { useState } from 'react';
 
 function GalleryItem({ gallerys, galleryRefreshCallBack }) {
     //Axios PUT call by id
@@ -18,15 +18,22 @@ function GalleryItem({ gallerys, galleryRefreshCallBack }) {
                 console.error('ERROR:', err);
             });
     };
-
+    //Toggle picture handler
+    const [togglepicture, setTogglePicture] = useState(false);
     //No Delete function
-
     return (
         <div data-testid="galleryItem">
-            <h2>My Gallery</h2>
+            <div data-testid="toggle" onClick={() => setTogglePicture(!togglepicture)}>
+                {togglepicture ? (
+                <p data-testid="description">
+                {gallerys.description}
+                </p>
+                ) : (
+                <img src={gallerys.url} alt={gallerys.title}/>
+                )}
+            </div>
             <div>
                 <p>{gallerys.title}</p>
-                <img src={gallerys.url} alt={gallerys.title}/>
                 <button data-testid="like" onClick={(event) => {
                     event.preventDefault();
                     handleClickloveit(gallerys.id);
